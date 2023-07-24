a81c7b88fb0008af0987868896a1818e95e63a87
Ayush Singh <ayushkrsingh1184@gmail.com>
2023-07-24 16:34:57 +0530

    Adding the toWatch Section

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App v1.js';
 // import App from './App v2.js';
-import App from './App v3.js';
-// import App from './App v4.js';
+// import App from './App v3.js';
+import App from './App v4.js';
 
 const root = ReactDOM.createRoot(document.querySelector(`#root`));
 root.render(
