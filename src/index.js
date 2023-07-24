3f37127d45767f1f2399fa0e84e3c52916b2f893
Ayush Singh <ayushkrsingh1184@gmail.com>
2023-07-24 22:16:59 +0530

    Updated files Section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App v1.js';
-// import App from './App v2.js';
-// import App from './App v3.js';
-import App from './App v4.js';
+
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.querySelector(`#root`));
 root.render(
